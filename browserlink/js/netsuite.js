fdfdfbfb277544296cf9cc695bdb0c3989fa8e42
Kevin Mershon <kevin@mershonenterprises.com>
2018-04-11 15:47:43 -0700

    Fix refresh issue, switch back to editor when done

--- browserlink/js/netsuite.js
+++ browserlink/js/netsuite.js
@@ -3,14 +3,29 @@ function runScript(scriptBody) {
   if (scriptBody.indexOf('require(') < 0) {
     document.querySelector('#inpt_runtimeversion1').title = '1.0';
     document.querySelector('#inpt_runtimeversion1').value = '1.0';
+    document.querySelector('#hddn_runtimeversion1').value = '1.0';
   } else {
     document.querySelector('#inpt_runtimeversion1').title = '2.0';
     document.querySelector('#inpt_runtimeversion1').value = '2.0';
+    document.querySelector('#hddn_runtimeversion1').value = '2.0';
   }
+  document.querySelector('#mainscript').focus();
   document.querySelector('#mainscript').value = scriptBody;
 
-  window.debugScript('adhoc');
+  document.querySelector('#debug').click();
   setTimeout(function() {
-    window.debugCommand('go');
-  }, 50);
+    document.querySelector('#continue').click();
+
+    function waitForSwitchToEditor() {
+      var switchToEditorButton = document.querySelector('#showeditor');
+      if (!switchToEditorButton) {
+        setTimeout(waitForSwitchToEditor, 50);
+      } else {
+        switchToEditorButton.click();
+      }
+    }
+    setTimeout(waitForSwitchToEditor, 50);
+
+  }, 150);
+
 }
